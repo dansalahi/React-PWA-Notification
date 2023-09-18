41d8bfb0f2c7898a8e5a9513f413ec8db388954f
Daniel Salahi <dan.salahii@gmail.com>
2023-09-18 17:11:00 +0300

    updated app screen

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,8 @@ import { HomeScreen } from "screens";
 
 const App = () => {
   return (
-    <div className="bg-slate-200">
-      <h1>PWA Notification TESTING</h1>
+    <div className="p-5 mx-auto">
+      <h1 className="font-semibold text-center">PWA Notification TESTING</h1>
       <HomeScreen/>
     </div>
   );
